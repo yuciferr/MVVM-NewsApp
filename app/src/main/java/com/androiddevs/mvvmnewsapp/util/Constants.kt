74355809a846d6b7a0e0c0ebe7fff3b3fb83614a
Yusuf Samed Çelik <ysfsmdc@gmail.com>
2022-10-08 16:59:55 +0300

    Handling Search News Response

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -5,5 +5,6 @@ class Constants {
     companion object{
         const val API_KEY = "b7f7e6f191fa4b0f9c96b0cced74a8fd"
         const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
